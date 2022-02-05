c72612b96502dc4b091d1072a4dd513b90070aef
Johan <johan@romantech.net>
2022-02-06 04:07:51 +0900

    :bulb: :triangular_flag_on_post: Feat: 404 페이지 및 리다이렉트 기능 추가

---
+++ pages/404.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import Container from 'components/Container';
+import { Button, Result } from 'antd';
+import { useRouter } from 'next/router';
+
+export default function Custom404() {
+  const [seconds, setSeconds] = useState(5);
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setInterval(() => setSeconds(prev => prev - 1), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    if (seconds === 0) router.replace('/');
+  }, [router, seconds]);
+
+  return (
+    <Container>
+      <Result
+        status="404"
+        title="페이지를 찾을 수 없어요!"
+        subTitle={`${seconds}초 뒤 홈 화면으로 이동합니다`}
+        extra={
+          <Button type="primary" onClick={() => router.replace('/')}>
+            Back Home
+          </Button>
+        }
+      />
+    </Container>
+  );
+}
