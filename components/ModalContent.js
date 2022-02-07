9803da5d72eebe7dff71e3724a23c494332a59f4
Johan <johan@romantech.net>
2022-02-07 20:45:54 +0900

    :lipstick: 모달 콘텐츠 반응형 대응

--- components/ModalContent.js
+++ components/ModalContent.js
@@ -6,11 +6,12 @@ export default function ModalContent({ store }) {
   const splitDesc = description.split(/(\n)/g);
   const category = splitDesc.shift().trim();
   const lastOrder = splitDesc.pop().trim();
+  const desc = splitDesc.slice(3);
   const iconStyle = { position: 'relative', top: '-4px' };
 
   return (
-    <div className="p-8 flex h-full w-full justify-evenly gap-10">
-      <section className="relative h-full w-[45%] bg-gray-200">
+    <div className="p-8 flex flex-col lg:flex-row h-full w-full justify-evenly gap-10">
+      <section className="relative h-full w-full lg:w-[45%] bg-gray-200">
         <Image
           className="object-cover max-h-28"
           src={image}
@@ -18,22 +19,22 @@ export default function ModalContent({ store }) {
           layout="fill"
         />
       </section>
-      <section className="w-[50%] h-full overflow-auto pr-4">
-        <header className="h-[20%] mb-4">
+      <section className="relative w-full lg:w-[50%] h-full overflow-auto pr-4">
+        <header className="lg:mb-4">
           <span className="font-medium inline-block pb-1 mb-4 border-b-4 border-black">
             {category}
           </span>
-          <h1 className="text-5xl uppercase">{name}</h1>
+          <h1 className="text-3xl lg:text-5xl uppercase">{name}</h1>
         </header>
-        <article className="flex flex-col text-lg h-[77%] justify-between">
-          <section className="whitespace-pre-line">{splitDesc}</section>
-          <section>
-            <div className="border-t border-b pt-2 pb-2 text-base">
+        <article className="flex flex-col text-base lg:text-lg">
+          <section className="whitespace-pre-line grow">{desc}</section>
+          <section className="lg:absolute lg:bottom-0 lg:w-full">
+            <div className="border-t border-b pt-2 pb-2">
               <ClockCircleOutlined style={iconStyle} />
               <span className="ml-2">{lastOrder}</span>
             </div>
             {url && (
-              <div className="border-b pt-2 pb-2 text-base">
+              <div className="border-b pt-2 pb-2">
                 <HomeOutlined style={iconStyle} />
                 <a className="ml-2" href={url} target="_blank" rel="noreferrer">
                   {url}
