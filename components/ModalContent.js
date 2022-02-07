754bd0cbf3cb7402f925c8ce6046c40cfa4d66e4
Johan <johan@romantech.net>
2022-02-07 16:14:19 +0900

    :lipstick: 모달 콘텐츠 디자인 수정

--- components/ModalContent.js
+++ components/ModalContent.js
@@ -1,34 +1,48 @@
 import Image from 'next/image';
-import { HomeOutlined } from '@ant-design/icons';
+import { ClockCircleOutlined, HomeOutlined } from '@ant-design/icons';
 
 export default function ModalContent({ store }) {
   const { name, description, url, image } = store;
-  console.log(description.split('\n\n'));
+  const splitDesc = description.split(/(\n)/g);
+  const category = splitDesc.shift().trim();
+  const lastOrder = splitDesc.pop().trim();
+  const iconStyle = { position: 'relative', top: '-4px' };
+
   return (
-    <section className="p-8 flex h-full w-full justify-evenly gap-10">
-      <div className="relative h-full w-[45%] bg-black">
+    <div className="p-8 flex h-full w-full justify-evenly gap-10">
+      <section className="relative h-full w-[45%] bg-gray-200">
         <Image
           className="object-cover max-h-28"
           src={image}
           alt="food-background"
           layout="fill"
         />
-      </div>
-      <div className="w-[50%] h-full overflow-auto">
-        <div className="w-[100px] border-4 border-t mb-3 border-black" />
-        <h1 className="text-5xl uppercase h-[20%] m-0">{name}</h1>
-        <div className="flex flex-col text-lg h-[77%] justify-between">
-          <p className="whitespace-pre-line">{description}</p>
-          {url && (
-            <div className="border-t pt-2">
-              <HomeOutlined style={{ position: 'relative', top: '-4px' }} />
-              <a className="ml-2" href={url} target="_blank" rel="noreferrer">
-                {url}
-              </a>
+      </section>
+      <section className="w-[50%] h-full overflow-auto">
+        <header className="h-[20%] mb-4">
+          <span className="font-medium inline-block pb-1 mb-4 border-b-4 border-black">
+            {category}
+          </span>
+          <h1 className="text-5xl uppercase">{name}</h1>
+        </header>
+        <article className="flex flex-col text-lg h-[77%] justify-between">
+          <section className="whitespace-pre-line">{splitDesc}</section>
+          <section>
+            <div className="border-t border-b pt-2 pb-2 text-base">
+              <ClockCircleOutlined style={iconStyle} />
+              <span className="ml-2">{lastOrder}</span>
             </div>
-          )}
-        </div>
-      </div>
-    </section>
+            {url && (
+              <div className="border-b pt-2 pb-2 text-base">
+                <HomeOutlined style={iconStyle} />
+                <a className="ml-2" href={url} target="_blank" rel="noreferrer">
+                  {url}
+                </a>
+              </div>
+            )}
+          </section>
+        </article>
+      </section>
+    </div>
   );
 }
