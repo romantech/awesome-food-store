ef4c9d54c68388971656127369b01c76425103cb
Johan <johan@romantech.net>
2022-02-09 19:17:40 +0900

    :rocket: Deploy: 배포 테스트

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function middleware(req, _ev) {
+  console.log(req);
   const { pathname } = req.nextUrl;
   if (pathname === '/') {
     return NextResponse.redirect('/stores');
