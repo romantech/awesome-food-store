e007ca368d391dbb8c415b79f9f41b5b5fa4f471
Johan <johan@romantech.net>
2022-02-09 23:34:43 +0900

    :memo: Fix: 오픈 그래프 설명 수정

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,9 @@ if (typeof document === 'undefined') {
   React.useLayoutEffect = React.useEffect;
 }
 
+const description =
+  'NextJS를 이용해 만든 간단한 맛집 사이트입니다. NextJS의 정적 생성(Static Generation) 방식으로 구현했으며, 모달창에도 하위 경로가 붙는 인스타그램 스타일 모달 라우팅을 적용했습니다.';
+
 // _document.js 역할 : HTMl 문서 전체 설정(meta 태그 등) / 전체 페이지에 적용됨
 // _document.js 에선 React Lifecycle Hook, Data Fetching 사용 불가
 // _document.js 의 콘솔은 서버에서만 볼 수 있음
@@ -16,9 +19,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta content="white" name="theme-color" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1, shrink-to-fit=no"
+        />
         <meta name="theme-color" content="#28231c" />
-        <meta name="description" content="NextJS 스터디 미니 프로젝트" />
+        <meta name="description" content={description} />
         <link rel="icon" href="/favicon.png" />
         {/* 오픈그래프 */}
         <meta property="og:type" content="website" />
@@ -27,24 +33,9 @@ export default function Document() {
           property="og:image"
           content="https://user-images.githubusercontent.com/8604840/153217433-8c9572c6-6fa9-4646-b976-2a10de8d6b0e.png"
         />
-        <meta property="og:description" content="NextJS 스터디 미니 프로젝트" />
+        <meta property="og:description" content={description} />
         <meta
           property="og:url"
-          content="https://awesome-food-store.vercel.app"
-        />
-        {/* 오픈그래프 소셜 */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="Awesome Food Store" />
-        <meta
-          name="twitter:description"
-          content="NextJS 스터디 미니 프로젝트"
-        />
-        <meta
-          name="twitter:image"
-          content="https://user-images.githubusercontent.com/8604840/153217433-8c9572c6-6fa9-4646-b976-2a10de8d6b0e.png"
-        />
-        <meta
-          name="twitter:domain"
           content="https://awesome-food-store.vercel.app"
         />
       </Head>
