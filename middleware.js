3e4c03e7935975426e02273c2fed00777beb7023
ColorFilter <johan@romantech.net>
2024-03-30 00:12:49 +0900

    📝 (FillLayoutImage.js): next/image를 next/legacy/image로 변경

--- middleware.js
+++ middleware.js
@@ -1,11 +1,19 @@
 import { NextResponse } from 'next/server';
 
+/**
+ * A middleware function that rewrites the URL to /stores.
+ * {@link https://nextjs.org/docs/pages/building-your-application/routing/middleware#example|for more information on Next.js middleware.}
+ *
+ * @param {object} req - the request object
+ * @param {any} _ev - the event parameter (unused)
+ * @return {object} the rewritten URL response
+ */
 export function middleware(req, _ev) {
   return NextResponse.rewrite(new URL('/stores', req.url));
 }
 
 export const config = {
-  /*
+  /**
    * 경로가 / 일 때만 middleware 함수 호출
    * @see https://nextjs.org/docs/pages/building-your-application/routing/middleware#example
    * */
