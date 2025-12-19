b73a6c604a176d5095e980fd95710e1174c3e8b9
ColorFilter <johan@romantech.net>
2025-12-19 11:30:02 +0900

    :package: 마이너 패키지 업데이트 및 next.config.js 유효성 검사 에러 수정 (#25)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const withAntdLess = require('next-plugin-antd-less');
 
-module.exports = withAntdLess({
+// 1. 플러그인 설정을 변수에 담습니다.
+const nextConfig = withAntdLess({
+  // --- 플러그인 전용 옵션 ---
   lessVarsFilePath: './styles/variables.less',
   lessVarsFilePathAppendToEndOfContent: false,
-  // optional https://github.com/webpack-contrib/css-loader#object
   cssLoaderOptions: {},
 
+  // --- Next.js 전용 옵션 ---
   images: {
     remotePatterns: [
       {
@@ -15,4 +17,19 @@ module.exports = withAntdLess({
       },
     ],
   },
+
+  webpack(config) {
+    return config;
+  },
 });
+
+// 2. Next.js가 인식하지 못해 에러를 뱉는 키들을 수동으로 삭제합니다.
+// (플러그인은 위에서 이미 이 값들을 읽어갔으므로 삭제해도 스타일 적용에는 문제없습니다)
+if (typeof nextConfig === 'object') {
+  delete nextConfig.lessVarsFilePath;
+  delete nextConfig.lessVarsFilePathAppendToEndOfContent;
+  delete nextConfig.cssLoaderOptions;
+}
+
+// 3. 깨끗해진 설정을 내보냅니다.
+module.exports = nextConfig;
